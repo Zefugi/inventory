75a2cb5902261363d8e56712c937f127b7d993bc
Zefugi <trowi@live.dk>
2020-10-12 18:46:43 +0200

    Wrote the initial test descriptors for Inventory.

---
+++ Zefugi.Inventory/Zefugi.Inventory.Tests/Inventory_Tests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory.Tests
+{
+    [TestFixture]
+    public class Inventory_Tests
+    {
+        [Test]
+        public void SlotsTotal_GetsNumberOfSlotsInTotal() { }
+
+        [Test]
+        public void SlotsTotal_SetThrowsException_IfSlotsInUse() { }
+
+        [Test]
+        public void HasRoom_ReturnsSlotAvailabilityForSpecifiedItem() { }
+
+        [Test]
+        public void IsAvailable_ReturnsItemAvailabilityForSpecifiedItem() { }
+
+        [Test]
+        public void GetFreeSlots_ReturnsTotalSlotsMinusSlotsUsed() { }
+
+        [Test]
+        public void GetUsedSlots_ReturnsSlotsUsed() { }
+
+        [Test]
+        public void Store_MakesItemAvailable_IfHasRoom() { }
+
+        [Test]
+        public void Drain_RemovesAndReturnsItem_IfAvailable() { }
+
+        [Test]
+        public void AutoStack_StacksItemsOfSameTypeInMaxStackSizes() { }
+    }
+}
