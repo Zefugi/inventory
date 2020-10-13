6e04d0e1d7aae4ab0de39c7da1180e9fd6b99e06
Zefugi <trowi@live.dk>
2020-10-13 19:28:05 +0200

    Created the iteminfo and repository model files with limited implementation.

---
+++ Zefugi.Inventory/Zefugi.Inventory/IRepositorySystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory
+{
+    public interface IRepositorySystem
+    {
+        IItemInfo this[int id] { get; }
+        int Count { get; }
+
+        void Clear();
+        void Add(IItemInfo item);
+        void AddRange(IEnumerable<IItemInfo> list);
+    }
+}
---
+++ Zefugi.Inventory/Zefugi.Inventory/RepositorySystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory
+{
+    public class RepositorySystem : IRepositorySystem
+    {
+        private Dictionary<int, IItemInfo> _items = new Dictionary<int, IItemInfo>();
+
+        public IItemInfo this[int id] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(IItemInfo item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<IItemInfo> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
