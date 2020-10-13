a821b0f71dd35f0bc3dd715e4e10a2bc4b484229
Zefugi <trowi@live.dk>
2020-10-13 19:40:46 +0200

    Added titles to the TODO comments.

--- Zefugi.Inventory/Zefugi.Inventory.Tests/RepositorySystem_Tests.cs
+++ Zefugi.Inventory/Zefugi.Inventory.Tests/RepositorySystem_Tests.cs
@@ -12,19 +12,19 @@ namespace Zefugi.Inventory.Tests
     [TestFixture]
     public class RepositorySystem_Tests
     {
-        [Test] // TODO
+        [Test] // TODO Add_MakesItemInfoAvailable
         public void Add_MakesItemInfoAvailable() { }
 
-        [Test] // TODO
+        [Test] // TODO Add_OverwritesItem_IfIdExists
         public void Add_OverwritesItem_IfIdExists() { }
 
-        [Test] // TODO
+        [Test] // TODO Clear_RemovesAllItems
         public void Clear_RemovesAllItems() { }
 
-        [Test] // TODO
+        [Test] // TODO Count_ReturnsNumberOfItems
         public void Count_ReturnsNumberOfItems() { }
 
-        [Test] // TODO
+        [Test] // TODO Indexer_ReturnsItem_IfAvailable_Else_ReturnsNull
         public void Indexer_ReturnsItem_IfAvailable_Else_ReturnsNull() { }
     }
 }
