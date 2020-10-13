65eb62d08fdc447018b1efe63d4127a82569acd8
Zefugi <trowi@live.dk>
2020-10-13 19:28:53 +0200

    Listed the basic tests for repository system.

---
+++ Zefugi.Inventory/Zefugi.Inventory.Tests/RepositorySystem_Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Zefugi.Inventory.Tests
+{
+    [TestFixture]
+    public class RepositorySystem_Tests
+    {
+        [Test] // TODO
+        public void Add_MakesItemInfoAvailable() { }
+
+        [Test] // TODO
+        public void Add_OverwritesItem_IfIdExists() { }
+
+        [Test] // TODO
+        public void Clear_RemovesAllItems() { }
+
+        [Test] // TODO
+        public void Count_ReturnsNumberOfItems() { }
+
+        [Test] // TODO
+        public void Indexer_ReturnsItem_IfAvailable_Else_ReturnsNull() { }
+    }
+}
