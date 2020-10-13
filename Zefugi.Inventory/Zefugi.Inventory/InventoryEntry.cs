fc39236f97640bfa399bcf158dee92e1429b88c9
Zefugi <trowi@live.dk>
2020-10-13 11:24:29 +0200

    Created the basic classes

---
+++ Zefugi.Inventory/Zefugi.Inventory/InventoryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory
+{
+    public class InventoryEntry
+    {
+        public Item Item { get; set; }
+
+        public int ItemCount { get; set; }
+    }
+}
---
+++ Zefugi.Inventory/Zefugi.Inventory/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory
+{
+    public struct Item
+    {
+        public long ID;
+
+        public int Slots;
+        public int StackSize;
+
+        public string Title;
+    }
+}
