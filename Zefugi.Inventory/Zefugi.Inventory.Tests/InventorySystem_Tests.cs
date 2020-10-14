503c4bb7fdbf20a73e628741fa03f1465cc7e815
Zefugi <trowi@live.dk>
2020-10-14 11:00:20 +0200

    Listed the basic tests for InventorySystem

---
+++ Zefugi.Inventory/Zefugi.Inventory.Tests/InventorySystem_Tests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory.Tests
+{
+    [TestFixture]
+    public class InventorySystem_Tests
+    {
+        [Test] // TODO TotalSlots_CanGrow
+        public void TotalSlots_CanGrow() { }
+
+        [Test] // TODO TotalSlots_CanShrink_IfSpaceAvailable
+        public void TotalSlots_CanShrink_IfSpaceAvailable() { }
+
+        [Test] // TODO TotalSlots_Throws_WhenShrink_IfSpaceUnavailable
+        public void TotalSlots_Throws_WhenShrink_IfSpaceUnavailable() { }
+
+        [Test] // TODO Store_MakesItemAvailable_IfSpaceAvailable
+        public void Add_MakesItemAvailable_IfSpaceAvailable() { }
+
+        [Test] // TODO Store_Throws_IfSpaceUnavailable
+        public void Store_Throws_IfSpaceUnavailable() { }
+
+        [Test] // TODO Store_Compresses_IfAutoStackIsTrue
+        public void Store_Compresses_IfAutoStackIsTrue() { }
+
+        [Test] // TODO Retrieve_ReturnsItemAndMakesItemUnavailable_IfItemIsAvailable
+        public void Retrieve_ReturnsItemAndMakesItemUnavailable_IfItemIsAvailable() { }
+
+        [Test] // TODO Retrieve_Throws_ifItemIsUnavailable
+        public void Retrieve_Throws_ifItemIsUnavailable() { }
+
+        [Test] // TODO Retrieve_Compresses_IfAutoStackIsTrue
+        public void Retrieve_Compresses_IfAutoStackIsTrue() { }
+
+        [Test] // TODO UsedSlots_ReturnsUsedSlots
+        public void UsedSlots_ReturnsUsedSlots() { }
+
+        [Test] // TODO FreeSlots_ReturnsUnusedSlots
+        public void FreeSlots_ReturnsUnusedSlots() { }
+
+        [Test] // TODO Clear_ReturnsAllItemsAndMakesThemUnavailable
+        public void Clear_ReturnsAllItemsAndMakesThemUnavailable() { }
+
+        [Test] // TODO IsAvailable_ReturnsTrue_OnlyIfTheSpecifiedItemsAreAvailable
+        public void IsAvailable_ReturnsTrue_OnlyIfTheSpecifiedItemsAreAvailable() { }
+
+        [Test] // TODO AutoStack_StacksItemsToSaveSlots
+        public void Compress_StacksItemsToSaveSlots() { }
+    }
+}
