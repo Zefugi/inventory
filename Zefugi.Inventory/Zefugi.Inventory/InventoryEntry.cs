372aea02a9cd83c958cff3750b9e7f1f8c5fecc4
Zefugi <trowi@live.dk>
2020-10-14 10:42:11 +0200

    Created the InventoryEntry and InventorySyste classes.

---
+++ Zefugi.Inventory/Zefugi.Inventory/InventoryEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory
+{
+    public class InventoryEntry
+    {
+        public IItemInfo ItemInfo { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
---
+++ Zefugi.Inventory/Zefugi.Inventory/InventorySystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zefugi.Inventory
+{
+    public class InventorySystem
+    {
+        private List<InventoryEntry> _items = new List<InventoryEntry>();
+
+        private int _totalSlots = 1;
+    }
+}
